ed63a3f9edbda8890e0af61e0a5e6e1b392ecdfd
sherdorus <sherdor@ymail.com>
2025-08-23 23:23:05 +0500

    feat: add AppShellConfig with custom theme setup
    - Introduced AppShellConfig implementing AppShellConfigurator
    - Applied custom "todo" theme with Lumo dark variant

--- src/main/java/io/sherdor/todoapp/config/AppShellConfig.java
+++ src/main/java/io/sherdor/todoapp/config/AppShellConfig.java
@@ -1,5 +1,9 @@
 package io.sherdor.todoapp.config;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 
-public class AppShellConfig {
+@Theme(value = "todo", variant = Lumo.DARK)
+public class AppShellConfig implements AppShellConfigurator {
 }
