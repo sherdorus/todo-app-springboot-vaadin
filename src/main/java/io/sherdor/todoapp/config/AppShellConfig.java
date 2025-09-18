6490517d86edc0a01db2e5e70031724bd1915db0
sherdorus <sherdor@ymail.com>
2025-09-18 14:59:03 +0500

    Fix Vaadin theme config: use default Lumo Dark instead of custom 'todo'

--- src/main/java/io/sherdor/todoapp/config/AppShellConfig.java
+++ src/main/java/io/sherdor/todoapp/config/AppShellConfig.java
@@ -4,6 +4,6 @@ import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
 
-@Theme(value = "todo", variant = Lumo.DARK)
+@Theme(variant = Lumo.DARK)
 public class AppShellConfig implements AppShellConfigurator {
 }
